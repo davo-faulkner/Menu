c15a5ed539b8aeaa35c1201211e62e4b87079062
Davo Faulkner <d@davo.co>
2016-08-28 18:47:27 -0500

    Print log messages - runs

--- app/src/main/java/com/example/android/menu/MainActivity.java
+++ app/src/main/java/com/example/android/menu/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.android.menu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void printToLogs(View view) {
         // Find first menu item TextView and print the text to the logs
+        TextView menuItem1 = (TextView) findViewById(R.id.menu_item_1);
+        String menuItem1String = menuItem1.getText().toString();
+        Log.i("MainActivity", menuItem1String);
 
         // Find second menu item TextView and print the text to the logs
+        TextView menuItem2 = (TextView) findViewById(R.id.menu_item_2);
+        String menuItem2String = menuItem2.getText().toString();
+        Log.i("MainActivity", menuItem2String);
 
         // Find third menu item TextView and print the text to the logs
+        TextView menuItem3 = (TextView) findViewById(R.id.menu_item_3);
+        String menuItem3String = menuItem3.getText().toString();
+        Log.i("MainActivity", menuItem3String);
 
     }
 }
